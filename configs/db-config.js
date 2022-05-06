e1a697ba313439600527a642177057d7b75e0c2c
Social Codia <socialcodia@gmail.com>
2022-05-06 07:54:08 +0530

    Backend source code for TargetManagement

--- configs/db-config.js
+++ configs/db-config.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-const dbConnection = () =>
-{    
-    mongoose.connect('mongodb+srv://socialcodia:%40Umair4Firdos%40@socialcodia.j8mwr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-    .then(()=>console.log('Database Connection Successfull'))
-    .catch(err=>console.log('Failed To Connect With Database, \nReason : '+err.message))
+const dbConnection = () => {
+    mongoose.connect('mongodb://localhost:27017/emp')
+        .then(() => console.log('Database Connection Successfull'))
+        .catch(err => console.log('Failed To Connect With Database, \nReason : ' + err.message))
 }
 
 module.exports = dbConnection;
